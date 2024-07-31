91c9ca9572ad4fccd8975444efce70eb3196c182
fathimohamed <mohafathi24@gmail.com>
2024-07-31 12:55:26 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,10 @@
-import React from "react";
-
-function About() {
-  return (
-    <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
-  );
-}
-
-export default About;
+ import React from "react";
+ import Links from "./Links"; 
+ function About({ bio, links, id }) { 
+return (
+ <div id={id}> {bio && bio !== "" && <p>{bio}</p>} 
+ <Links github={links.github} linkedin={links.linkedin} /> 
+ </div> 
+ );
+  } 
+  export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,15 @@
 import React from "react";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import About from "./About";
-
-// pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
-
-function App() {
-  return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
-    </div>
+ import NavBar from "./NavBar"; 
+ import Home from "./Home"; 
+ import About from "./About"; 
+ import user from "../data/user"; 
+ function App() {
+ return (
+ <div>
+ <NavBar />
+  <Home id="home" name={user.name} city={user.city} color={user.color} /> 
+  <About id="about" bio={user.bio} links={user.links} /> 
+  </div> 
   );
-}
-
-export default App;
+ }
+  export default App;
